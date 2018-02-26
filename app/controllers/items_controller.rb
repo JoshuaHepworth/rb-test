ca72860afad2292a04f87497f4dfb113c4c0e934
Filip Tatarnik <ftatarnik@gmail.com>
2018-02-25 20:52:53 -0600

    bullshit

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -69,6 +69,6 @@ class ItemsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def item_params
-      params.require(:item).permit(:item_number, :sap_number, :tr_number, :rb_number, :name, :machine, :material, :finish, :lead_time, :additional)
+      params.require(:item).permit(:item_number, :sap_number, :tr_number, :rb_number, :name, :machine, :material, :finish, :lead_time, :additional, :avatar)
     end
 end
